7b1cdfdeb98a6ad781006b322f67bb311a0687e9
artemutin@yandex.ru <artemutin@yandex.ru>
2015-11-10 14:21:33 +1000

    start static filling and match table parse

--- LiveScore.js
+++ LiveScore.js
@@ -24,4 +24,34 @@ $( document ).ready(function() {
     }, function(){
         $(".but.close-x").click();
     })
+
+    console.log($(".field-container i.ico.info2").map(function(index, elm){
+        return elm.getAttribute("data-id");
+    }).get().toString())
+
+    getPlayerName = function(data_id){
+        var name;
+        var url = "http://www.sports.ru/fantasy/basketball/player/info/150/"
+        $.get(url+data_id+".html", null, function(data){
+            var profileURL = $( data ).find("a.go-to-page").attr("href");
+            return $.get(profileURL, null, function(data){
+                name = $( data ).find("div.short-info div.descr").html()//sss;
+            });
+        });
+        return name;
+    }
+
+    $("i.ico.info2").click(function(){
+      var data_id = $("i.ico.info2").attr("data-id");
+      getPlayerName(data_id);
+    })
+
+    //stat table
+    var matchRows = $("div.stat.mB20 > table.stat-table tr");
+    function scanMatches(rows, myTeams){
+
+    }
+
+
 })
+
