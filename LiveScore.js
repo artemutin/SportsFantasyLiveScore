ae21eac2209cc31fbb9905e8f86d907e97408009
artemutin@yandex.ru <artemutin@yandex.ru>
2015-11-14 14:16:11 +1000

    rewrite extension code. Begin with sports page data parse

--- LiveScore.js
+++ LiveScore.js
@@ -1,57 +1,32 @@
-/**
- * Created by chichi on 11/5/15.
- */
-$( document ).ready(function() {
-    alert("Fuck you again");
-    var HOVER_DELAY = 1000;
-    //show player info on hover of <i> icon
-    $("i.ico.info2").hover(function(){
-        setTimeout(function(wasHovered, cssSelector){
-            var nowHovered = $(cssSelector);
-            wasHovered.is(nowHovered) ? nowHovered.trigger('click') : null;
+$({
+    var playerBoxes = $("ins.player.hold");
 
-        }, HOVER_DELAY, $("i:hover"), "i:hover");
-    },function(){
+    var players = playerBoxes.map(function(idx, elem){
+        elem = $(elem);
+        return {"team" : elem.find("img.t-shirt").attr("title"),
+            "data-id" : elem.find("i.ico.info2").attr("data-id"),
+            "elem" : elem
         }
-    )
-    //close player info overlay just by click to empty space
-    $(".overlay").click(function(){
-        $(".but.close-x").click();
-    })
-    //close player info overlay just by left hover of block
-    $(".person-popup").delay(HOVER_DELAY).hover(function(){
+    });
 
-    }, function(){
-        $(".but.close-x").click();
-    })
+    var matches = $("div.stat.mB20 table.stat-table tr").map(function(idx, elem){
+        elem = $(elem);
+        return {"dateStr" : elem.find("td").first().text(),
+            "homeTeam" : elem.find("td.owner-td").text().trim(),
+            "awayTeam" : elem.find("td.guests-td").text().trim()
+        }
+    });
 
-    console.log($(".field-container i.ico.info2").map(function(index, elm){
-        return elm.getAttribute("data-id");
-    }).get().toString())
+    function getPlayerNumber(player){
+        var url = "http://www.sports.ru/fantasy/basketball/player/info/150/"+player["data-id"]+".html";
+        var playerNum;
+        $.get(url, null, function(htmlData,resp){
+            console.log(resp)
+            playerNum = $(htmlData).find(".profile-table tr").first().find("td").first().text();
+            console.log(playerNum)
+            player.num = new RegExp(/\([\D]*([\d]+)[\D]*\)/).exec(playerNum)[1];
+        }, "html");
+        return playerNum;
+    };
 
-    getPlayerName = function(data_id){
-        var name;
-        var url = "http://www.sports.ru/fantasy/basketball/player/info/150/"
-        $.get(url+data_id+".html", null, function(data){
-            var profileURL = $( data ).find("a.go-to-page").attr("href");
-            return $.get(profileURL, null, function(data){
-                name = $( data ).find("div.short-info div.descr").html()//sss;
-            });
-        });
-        return name;
-    }
-
-    $("i.ico.info2").click(function(){
-      var data_id = $("i.ico.info2").attr("data-id");
-      getPlayerName(data_id);
-    })
-
-    //stat table
-    var matchRows = $("div.stat.mB20 > table.stat-table tr");
-    function scanMatches(rows, myTeams){
-
-    }
-
-
-})
-
+});
